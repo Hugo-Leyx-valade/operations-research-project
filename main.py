8ab9123044e4fb42c32f6dc77918041f74b15579
Victor <victor.dallon@efrei.net>
2025-03-23 17:59:03 +0100

    First commit - main test

---
+++ main.py
@@ -0,0 +1,11 @@
+
+def print_test(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Ceci est un, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_test('test')
+
+
