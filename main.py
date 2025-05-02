9ac2655f7c80f7ee508b162255acae96552b9022
hugol <hugo.leyx-valade@efrei.net>
2025-05-02 20:08:11 +0200

    tugykfftyjt

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from functions import *
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    imported_data = graph_import("graphs/graph1.txt")
+    imported_data = graph_import("graphs/graph11.txt")
     graph = imported_data[0]
     flow_type = imported_data[1]
     if flow_type == 1:
@@ -14,8 +14,10 @@ if __name__ == "__main__":
 
     if flow_type == 2:  # flot max
         print_graph_to_matrix_of_values(graph)
+        print("\n =========== ford fulkerson ===========\n")
         ff_mat = ford_fulkerson(graph)[1]
         compute_flow_matrix(graph, ff_mat)
-        pl = push_relabel(graph,1,len(graph)-1)
+        print("\n =========== push-label ===========\n")
+        pl = push_relabel(graph,0,len(graph)-1)
         print('\n valeurs du poussé réétiqueté : ' + str(pl))
 
